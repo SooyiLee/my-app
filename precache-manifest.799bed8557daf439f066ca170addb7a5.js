8a7ff916d6280f591b4df36a8806ef62c08e2c89
SooyiLee <bysoo1982@gmail.com>
2019-03-06 18:23:17 +0900

    Updates

---
+++ precache-manifest.799bed8557daf439f066ca170addb7a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "f93d22963b591af98008",
+    "url": "/my-app/static/js/main.f93d2296.chunk.js"
+  },
+  {
+    "revision": "dbe48be8e063b7b7c522",
+    "url": "/my-app/static/js/2.dbe48be8.chunk.js"
+  },
+  {
+    "revision": "f93d22963b591af98008",
+    "url": "/my-app/static/css/main.b66cac17.chunk.css"
+  },
+  {
+    "revision": "614825d236c1301f42e91100b1eac9e5",
+    "url": "/my-app/index.html"
+  }
+];
